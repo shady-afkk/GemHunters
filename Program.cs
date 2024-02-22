6c0184fce9b2d3265dbdf02b7c32d20d5e36f9dd
shady-afkk <156012612+shady-afkk@users.noreply.github.com>
2024-02-22 15:45:55 -0500

    update

--- Program.cs
+++ Program.cs
@@ -200,7 +200,7 @@ namespace GemHunters
             }
             else
             {
-                Console.WriteLine("Invalid move. Player loses a turn now.");
+                Console.WriteLine("Invalid move . Player loses a turn now.");
             }
         }
 
